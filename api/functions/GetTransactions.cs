076a77a2634a50adfb87f09c7ae62f30cde7a545
AamerAbuArja <aamerabuarja@yahoo.com>
2025-10-30 21:43:07 +0300

    Edited C# Function 5

--- api/functions/GetTransactions.cs
+++ api/functions/GetTransactions.cs
@@ -1,6 +1,8 @@
+using System.Net; // here
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http; // here
 using Microsoft.Extensions.Logging;
 
 namespace api.Functions;
@@ -15,9 +17,19 @@ public class GetTransactions
     }
 
     [Function("GetTransactions")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync("Welcome to Azure Functions!");
+        return response;
     }
+
+
+    // [Function("GetTransactions")]
+    // public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    // {
+    //     _logger.LogInformation("C# HTTP trigger function processed a request.");
+    //     return new OkObjectResult("Welcome to Azure Functions!");
+    // }
 }
