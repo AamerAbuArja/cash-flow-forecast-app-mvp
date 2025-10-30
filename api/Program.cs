dd92cf5f29caec25e7c314e0ac5272273ba9b054
AamerAbuArja <aamerabuarja@yahoo.com>
2025-10-30 21:55:52 +0300

    Edited C# Function 6 with more changes

--- api/Program.cs
+++ api/Program.cs
@@ -1,14 +1,32 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core; // here
+using Microsoft.Azure.Cosmos; // here
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder; // here
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
 builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+	.AddApplicationInsightsTelemetryWorkerService()
+	.ConfigureFunctionsApplicationInsights();
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string connectionString = Environment.GetEnvironmentVariable("CosmosDBConnectionString");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+CosmosClient cosmosClient = new CosmosClient(connectionString);
+builder.Services.AddSingleton(cosmosClient);
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+		options.AllowSynchronousIO = true;
+});
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
 
 builder.Build().Run();
--- api/functions/GetTransactions.cs
+++ api/functions/GetTransactions.cs
@@ -1,28 +1,79 @@
 using System.Net; // here
+using api.Models; // here 2
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos; // here 2
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http; // here
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json; // here 2
 
 namespace api.Functions;
 
 public class GetTransactions
 {
     private readonly ILogger<GetTransactions> _logger;
-
-    public GetTransactions(ILogger<GetTransactions> logger)
+	private readonly CosmosClient _cosmosClient;
+	private readonly Container _container;
+    public GetTransactions(ILogger<GetTransactions> logger, CosmosClient client)
     {
         _logger = logger;
+		_cosmosClient = client;
+		_container = _cosmosClient.GetContainer("CashflowDB", "Transactions");
     }
 
     [Function("GetTransactions")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        await response.WriteAsJsonAsync("Welcome to Azure Functions!");
-        return response;
+        var response = req.CreateResponse();
+		try
+		{
+			var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+			string projectId = query["projectId"];
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+			if (string.IsNullOrWhiteSpace(projectId))
+			{
+				response.StatusCode = HttpStatusCode.BadRequest;
+				await response.WriteStringAsync("{\"error\":\"Missing query parameter: projectId\"}");
+				return response;
+			}
+
+			var sql = new QueryDefinition("SELECT * FROM c WHERE c.projectId = @projectId")
+					.WithParameter("@projectId", projectId);
+
+			var results = new List<Transaction>();
+			using var iterator = _container.GetItemQueryIterator<Transaction>(sql);
+			while (iterator.HasMoreResults)
+			{
+				var page = await iterator.ReadNextAsync();
+				results.AddRange(page);
+			}
+
+			response.StatusCode = HttpStatusCode.OK;
+			response.Headers.Add("Content-Type", "application/json");
+			await response.WriteStringAsync(JsonConvert.SerializeObject(results, Formatting.Indented));
+		}
+		catch (CosmosException ex)
+		{
+			response.StatusCode = (HttpStatusCode)ex.StatusCode;
+			await response.WriteStringAsync(JsonConvert.SerializeObject(new
+			{
+				error = ex.Message,
+				statusCode = ex.StatusCode
+			}));
+		}
+		catch (Exception ex)
+		{
+			response.StatusCode = HttpStatusCode.InternalServerError;
+			await response.WriteStringAsync(JsonConvert.SerializeObject(new
+			{
+				error = ex.Message
+			}));
+		}
+
+		return response;
     }
 
 
